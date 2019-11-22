1a22c8e15e56a8506135c4aada8b0e6a9be38ab3
Bernadeta <ursan.bernadeta@gmail.com>
2019-11-22 23:26:32 +0200

    Added FAB

--- app/src/main/java/com/example/electrictalk/Fragments/CarsFragment.java
+++ app/src/main/java/com/example/electrictalk/Fragments/CarsFragment.java
@@ -8,17 +8,29 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.electrictalk.Models.CarModel;
 import com.example.electrictalk.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarsFragment extends Fragment {
-    public CarsFragment() {
-        // Required empty public constructor
-    }
+
+    private List<CarModel> carList = new ArrayList<>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        carList.add(new CarModel("fage","FIAT",20));
+        carList.add(new CarModel("fage","FORD",40));
+        carList.add(new CarModel("fage","AUDI",50));
+
         return inflater.inflate(R.layout.fragment_cars, container, false);
+
     }
 
+    public CarsFragment() {
+        // Required empty public constructor
+    }
 }
