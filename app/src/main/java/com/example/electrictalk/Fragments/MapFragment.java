23a1daa252568699c5e63eadbbbe8dfa04085712
Cosmin <cirleacosmin@gmail.com>
2019-11-23 00:15:13 +0200

    added maps

--- app/src/main/java/com/example/electrictalk/Fragments/MapFragment.java
+++ app/src/main/java/com/example/electrictalk/Fragments/MapFragment.java
@@ -9,23 +9,35 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.electrictalk.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class MapFragment extends Fragment {
-
-
-    public MapFragment() {
-        // Required empty public constructor
-    }
-
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private MapView mMapView;
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map, container, false);
+        initializeViews(rootView);
+
+        mMapView.getMapAsync(this);
+        return rootView;
     }
 
+    public MapFragment() {
+
+    }
+
+    private void initializeViews(View rootView)
+    {
+        mMapView = rootView.findViewById(R.id.map_view);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap = googleMap;
+    }
 }
