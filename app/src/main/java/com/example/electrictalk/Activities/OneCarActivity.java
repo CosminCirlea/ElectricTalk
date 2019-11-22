6f0c318f651aa5a28e80e1eb0e0fb23a7f583034
Bernadeta <ursan.bernadeta@gmail.com>
2019-11-23 00:52:31 +0200

    Added One Car Activity

---
+++ app/src/main/java/com/example/electrictalk/Activities/OneCarActivity.java
@@ -0,0 +1,17 @@
+package com.example.electrictalk.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.electrictalk.R;
+
+public class OneCarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_car);
+    }
+
+}
