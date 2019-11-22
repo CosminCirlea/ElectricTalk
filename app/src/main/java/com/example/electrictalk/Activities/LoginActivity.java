2f6662d2760414f9c733030f20c5fa1ebbcd22a3
Bernadeta <ursan.bernadeta@gmail.com>
2019-11-22 19:12:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/electrictalk/Activities/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.electrictalk.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.electrictalk.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void OnSignUp(View view) {
+        Intent myInt2= new Intent(LoginActivity.this,SignUpActivity.class);
+        startActivity(myInt2);
+    }
+}
---
+++ app/src/main/java/com/example/electrictalk/Activities/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.electrictalk.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.electrictalk.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
