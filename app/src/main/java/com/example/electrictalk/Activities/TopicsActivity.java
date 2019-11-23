315e11697f48bcf6c0ee97a9017fd580e090267d
Bernadeta <ursan.bernadeta@gmail.com>
2019-11-23 18:21:14 +0200

    Added categories, topics, messages activities

---
+++ app/src/main/java/com/example/electrictalk/Activities/TopicsActivity.java
@@ -0,0 +1,16 @@
+package com.example.electrictalk.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.electrictalk.R;
+
+public class TopicsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topics);
+    }
+}
