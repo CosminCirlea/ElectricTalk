811253aed7862b5e7e32be0aac1a3a1ebc2ac410
Bernadeta <ursan.bernadeta@gmail.com>
2019-11-23 14:03:05 +0200

    profile update

--- app/src/main/java/com/example/electrictalk/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/electrictalk/Fragments/ProfileFragment.java
@@ -5,19 +5,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.electrictalk.R;
 
 public class ProfileFragment extends Fragment {
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
+
+    private EditText lastName, firstName;
+    private Button update;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        InitializeViews(view);
+        return view;
+    }
+
+    private void InitializeViews(View view){
+        lastName = view.findViewById(R.id.et_last_name);
+        firstName = view.findViewById(R.id.et_first_name);
+        update = view.findViewById(R.id.btn_edit);
     }
 
 }
