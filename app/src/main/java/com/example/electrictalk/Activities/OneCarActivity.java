35b6f578a519b1d7b2eec7bb5691c9eafd90ee7b
Cosmin <cirleacosmin@gmail.com>
2019-11-23 15:29:58 +0200

    wip on car details

--- app/src/main/java/com/example/electrictalk/Activities/OneCarActivity.java
+++ app/src/main/java/com/example/electrictalk/Activities/OneCarActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.electrictalk.Helpers.HttpClientManager;
 import com.example.electrictalk.R;
+import com.google.gson.Gson;
+
+import java.util.UUID;
 
 public class OneCarActivity extends AppCompatActivity {
+
+    private UUID carId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_car);
+
+        Gson gson = new Gson();
+        String aux = getIntent().getStringExtra("car_id");
+        carId = gson.fromJson(aux, UUID.class);
+
     }
 
 }
--- app/src/main/java/com/example/electrictalk/Adapters/CarsAdapter.java
+++ app/src/main/java/com/example/electrictalk/Adapters/CarsAdapter.java
@@ -1,15 +1,19 @@
 package com.example.electrictalk.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.electrictalk.Activities.OneCarActivity;
 import com.example.electrictalk.Models.CarModel;
 import com.example.electrictalk.R;
+import com.google.gson.Gson;
 
 import java.util.List;
+import java.util.UUID;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -40,10 +44,21 @@ public class CarsAdapter extends RecyclerView.Adapter<CarsAdapter.ViewHolder> {
         int batteryLeft = myCars.get(position).getBatteryLeft();
         String company = myCars.get(position).getCompany();
         String model = myCars.get(position).getModel();
+        final UUID carId = myCars.get(position).getId();
 
         holder.batteryLeftTv.setText(""+batteryLeft);
         holder.companyTv.setText(company);
         holder.modelTv.setText(model);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(context, OneCarActivity.class);
+                String carIdSerialized = new Gson().toJson(carId);
+                mIntent.putExtra("car_id",carIdSerialized);
+                context.startActivity(mIntent);
+            }
+        });
     }
 
     @Override
